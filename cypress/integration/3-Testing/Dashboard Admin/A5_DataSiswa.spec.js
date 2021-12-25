992ba780f8e40b6507e8bc1c58a14a619050b30b
fitriMD <mutiara.devi027@gmail.com>
2021-12-25 22:20:24 +0700

    data siswa tanpa no induk

--- cypress/integration/3-Testing/Dashboard Admin/A5_DataSiswa.spec.js
+++ cypress/integration/3-Testing/Dashboard Admin/A5_DataSiswa.spec.js
@@ -5,30 +5,29 @@ describe("Login Admin", function () {
             cy.visit("/login");
             cy.url().should('include', '/login');
             cy.get("#email").type(this.admin.email);
-            cy.get("#password").type(this.admin.password);
+            cy.get("#password", {timeout:10000}).type(this.admin.password);
             cy.get("#btn-login").click();
-            cy.wait(5000);
             cy.contains("Dashboard").should("be.visible");
             cy.contains("Admin").should("be.visible");
         });
     });
 
-    it("Tambah Data Kelas", function () {
-        cy.url().should('include','/');
+    // it("Tambah Data Kelas", function () {
+    //     cy.url().should('include','/');
     
-        cy.get("#liMasterData").click();
-        cy.get('#DataKelas').click();
-        cy.url().should('include','/kelas');
-        cy.contains("Data Kelas").should("be.visible");
-        cy.get("body > div > div.content-wrapper > section > div > div > div.col-md-12 > div > div.card-header > h3 > button").click()
-        cy.get("#nama_kelas").type("Duabelas B")
-        cy.get('#paket_id').select('Elektronika Industri', { force: true })
-        cy.get("#select2-guru_id-container").click().get('#guru_id').select('Devi', { force: true })
+    //     cy.get("#liMasterData").click();
+    //     cy.get('#DataKelas').click();
+    //     cy.url().should('include','/kelas');
+    //     cy.contains("Data Kelas").should("be.visible");
+    //     cy.get("body > div > div.content-wrapper > section > div > div > div.col-md-12 > div > div.card-header > h3 > button").click()
+    //     cy.get("#nama_kelas").type("Duabelas B")
+    //     cy.get('#paket_id').select('Elektronika Industri', { force: true })
+    //     cy.get("#select2-guru_id-container").click().get('#guru_id').select('Devi', { force: true })
     
-        cy.get("#form-kelas > div > div > div.modal-footer.justify-content-between > button.btn.btn-primary").click()
-    });
+    //     cy.get("#form-kelas > div > div > div.modal-footer.justify-content-between > button.btn.btn-primary").click()
+    // });
 
-    it.only('Tambah Data Siswa', function() {
+    it('Tambah Data Siswa tanpa mengisi Nomor Induk', function() {
         cy.url().should('include','/');
 
         cy.get("#MasterData").click();
@@ -38,16 +37,39 @@ describe("Login Admin", function () {
         cy.get("body > div.wrapper > div.content-wrapper > section > div > div > div.col-md-12 > div > div.card-header > h3 > button.btn.btn-default.btn-sm").click();
         cy.wait(400);
         cy.contains("Tambah Data Siswa").should("be.visible");
-        cy.get("#no_induk").type("3285");
-        cy.get("#nama_siswa").type("Fitri Mutiara Devi");
-        cy.get("#select2-jk-container").click().get("#jk").select('Perempuan',{force: true});
-        cy.get("#tmp_lahir").type("Gresik");
-        cy.get("#nis").type("0007817730");
+        cy.get("#no_induk").type(" ");
+        cy.get("#nama_siswa").type("Azafan Rizky Hidayat");
+        cy.get("#select2-jk-container").click().get("#jk").select('Laki-Laki',{force: true});
+        cy.get("#tmp_lahir").type("Malang");
+        cy.get("#nis").type("0000142537");
+        cy.get('#foto').attachFile('Fitri.png');
+        cy.get("#select2-kelas_id-container").click().get("#kelas_id").select('SEPULUH A',{force: true});
+        cy.get("#tgl_lahir").type("2001-08-14");
+        cy.get('body > div.wrapper > div.content-wrapper > section > div > div > div.modal.fade.bd-example-modal-lg.show > div > div > div.modal-footer.justify-content-between > button.btn.btn-primary').click();
+        cy.contains("The no induk field is required.", {timeout:10000}).should('be.visible')
+    });
 
-        cy.get("#select2-kelas_id-container").click().get("#kelas_id").select('DUABELAS A',{force: true});
-        cy.get("#tgl_lahir").type("2000-12-27");
+    it('Tambah Data Siswa', function() {
+        cy.url().should('include','/');
+
+        cy.get("#MasterData").click();
+        cy.get("#DataSiswa").click();
+        cy.contains("Data Siswa").should("be.visible");
+
+        cy.get("body > div.wrapper > div.content-wrapper > section > div > div > div.col-md-12 > div > div.card-header > h3 > button.btn.btn-default.btn-sm").click();
+        cy.wait(400);
+        cy.contains("Tambah Data Siswa").should("be.visible");
+        cy.get("#no_induk").type("7890");
+        cy.get("#nama_siswa").type("Azafan Rizky Hidayat");
+        cy.get("#select2-jk-container").click().get("#jk").select('Laki-Laki',{force: true});
+        cy.get("#tmp_lahir").type("Malang");
+        cy.get("#nis").type("0000142537");
+
+        cy.get("#select2-kelas_id-container").click().get("#kelas_id").select('SEPULUH A',{force: true});
+        cy.get("#tgl_lahir").type("2001-08-14");
         cy.get('#foto').attachFile('Fitri.png');
         cy.get('body > div.wrapper > div.content-wrapper > section > div > div > div.modal.fade.bd-example-modal-lg.show > div > div > div.modal-footer.justify-content-between > button.btn.btn-primary').click();
 
     });
+
 })
